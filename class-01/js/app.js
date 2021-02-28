d06b484117be480701d28e273a1d33ef6ed2ef3b
yahyaalbashar <v.salvatore7.gs@gmail.com>
2021-02-28 12:42:03 +0200

    todays review about web technologies

---
+++ class-01/js/app.js
@@ -0,0 +1,11 @@
+let userName='Cristiano ronaldo';
+let age=30;
+const tax=0.16;
+console.log("before changes: ",userName,age,tax);
+age=50;
+console.log("after changes: ",age,tax);
+
+alert('Hello everyone');
+confirm('Do you want to continue?');
+
+let userInput=prompt('enter you fav team: ');
