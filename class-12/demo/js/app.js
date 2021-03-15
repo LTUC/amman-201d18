63b6266f5883773b453397d8fb135ee238bd6428
yahyaalbashar <v.salvatore7.gs@gmail.com>
2021-03-15 12:03:00 +0200

    canvas and chart.js

--- class-12/demo/js/app.js
+++ class-12/demo/js/app.js
@@ -8,90 +8,108 @@ const names = [
   'smiling-goat',
   'sweater-goat',
 ];
-const leftImageEl = document.getElementById('left-image');
-const rightImageEl = document.getElementById('right-image');
-const imagesSection = document.getElementById('images-section');
-let remainingVotes = 5;
-function Goat(name) {
-  this.name = name;
-  this.path = `assets/${name}.jpg`;
-  this.votes = 0;
-  this.views = 0;
+const imageSection=document.getElementById('imagesSection');
+const leftImage=document.getElementById('leftImage');
+const rightImage=document.getElementById('rightImage');
+
+// let goats=[]
+function Goat(name){
+  this.name=name;
+  this.views=0;
+  this.votes=0;
+  this.path=`./assets/${name}.jpg`;
+  // goats.push(this);
   Goat.all.push(this);
 }
-Goat.all = [];
-for (let i = 0; i < names.length; i++) {
-  new Goat(names[i]);
-}
-function render() {
-  const leftIndex = randomNumber(0, Goat.all.length - 1);
-  const rightIndex = randomNumber(0, Goat.all.length - 1);
-  Goat.all[leftIndex].views++;
-  Goat.all[rightIndex].views++;
-  leftImageEl.src = Goat.all[leftIndex].path;
-  rightImageEl.src = Goat.all[rightIndex].path;
-  leftImageEl.alt = Goat.all[leftIndex].name;
-  rightImageEl.alt = Goat.all[rightIndex].name;
-  leftImageEl.title = Goat.all[leftIndex].name;
-  rightImageEl.title = Goat.all[rightIndex].name;
-}
-imagesSection.addEventListener('click', clickHandler);
-function clickHandler(event) {
-  remainingVotes--;
-  if (remainingVotes === 0) {
-    imagesSection.removeEventListener('click', clickHandler);
-    createChart();
-  } else {
-    if (event.target.id !== 'images-section') {
-      console.log('hi', event.target);
-      for (let i = 0; i < Goat.all.length; i++) {
-        if (Goat.all[i].name === event.target.title) {
-          Goat.all[i].votes++;
-        }
-      }
-      render();
-      console.log(Goat.all);
-    }
-  }
-}
+Goat.all=[];
 
-render();
-function createChart() {
-  const ctx = document.getElementById('myChart').getContext('2d');
-
-  const goatNames = [];
-  const goatVotes = [];
-  for (let i = 0; i < Goat.all.length; i++) {
-    goatNames.push(Goat.all[i].name);
-    goatVotes.push(Goat.all[i].votes);
-  }
-  console.log('Votes', goatVotes);
-  new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-      labels: goatNames,
-      datasets: [
-        {
-          barPercentage: 0.5,
-          // barThickness: 6,
-          borderWidth: 5,
-          label: '# of votes:',
-          backgroundColor: 'rgb(100, 125, 50)',
-          borderColor: 'rgb(0, 0, 0)',
-          data: goatVotes,
-        },
-      ],
-    },
-
-    // Configuration options go here
-    options: {},
-  });
+for(let i=0;i<names.length;i++){
+    new Goat(names[i]);
 }
+// console.table(Goat.all);
+// console.log(Goat.all);
 
 //helper functions
+// [0,1,2,3,4,5,6,7]
+function render(){
+  const leftIndex=randomNumber(0,Goat.all.length-1);
+  const leftRandomGoat=Goat.all[leftIndex];
+  leftImage.src=leftRandomGoat.path;
+  leftImage.title=leftRandomGoat.name;
+  leftImage.alt=leftRandomGoat.name;
+
+  const rightIndex=randomNumber(0,Goat.all.length-1);
+  const rightRandomGoat=Goat.all[rightIndex];
+  rightImage.src=rightRandomGoat.path;
+  rightImage.title=rightRandomGoat.name;
+  rightImage.alt=rightRandomGoat.name;
+
+  // console.log(leftImage);
+
+}
+
+imageSection.addEventListener('click',clickHandler);
+
+let maxTrials=5;
+
+function clickHandler(event){
+  maxTrials-=1;
+  if (event.target.id === 'leftImage' || event.target.id === 'rightImage'){
+    for(let i=0;i<Goat.all.length;i++){
+      if (Goat.all[i].name === event.target.title){
+        Goat.all[i].votes++;
+        console.table(Goat.all[i])
+      }
+    }
+    render();
+  }
+  if(maxTrials===0){
+    imageSection.removeEventListener('click',clickHandler);
+    createChart();
+  }
+}
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+function createChart(){
+  let context = document.getElementById('myChart').getContext('2d');
+  let getGoatsNames=[];
+  let getGoatsVotes=[];
+
+  for(let i=0;i<Goat.all.length;i++){
+    getGoatsNames.push(Goat.all[i].name);
+  }
+  for(let i=0;i<Goat.all.length;i++){
+    getGoatsVotes.push(Goat.all[i].votes);
+  }
+  let chartObject={
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+        labels:getGoatsNames,
+        datasets: [{
+            label: 'Goats voting results',
+            backgroundColor: 'rgb(100, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: getGoatsVotes
+        }
+      ]
+    },
+
+    // Configuration options go here
+    options: {
+      scales: {
+        xAxes: [{
+            barPercentage: 0.4
+        }]
+    }
+    }
+}
+  let chart = new Chart(context,chartObject);
+  
+}
+render();
