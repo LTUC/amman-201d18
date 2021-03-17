30b26a5c0962523544bc732a1f80fcc9b8d777ce
AseelHussein96 <aseelayed1996@gmail.com>
2021-03-17 15:43:20 +0200

    solve it

--- class-14/class-14-starter-code/js/catalog.js
+++ class-14/class-14-starter-code/js/catalog.js
@@ -14,7 +14,6 @@ function populateForm() {
   for (let i in Product.allProducts) {
     let option=document.createElement('option');
     option.textContent=Product.allProducts[i].name;
-    option.value=Product.allProducts[i].name;
     selectElement.appendChild(option);
     // console.log(Product.allProducts[i]);
   }
@@ -27,6 +26,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
+  event.preventDefult();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -39,17 +39,32 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  let item=document.getElementById('items').value;
   // TODO: get the quantity
+  let quantity=document.getElementById('quantity').value;
   // TODO: using those, add one item to the Cart
+  cart.addItem(item,quantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  let itemCount=document.getElementById('itemCount');
+  let total=JSON.parse(localStorage.getItem('total'));
+  itemCount.textContent=total;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
+  let items=document.getElementById('items').value;
+  let quantity=document.getElementById('quantity').value;
+
   // TODO: Add a new element to the cartContents div with that information
+  let cart=document.getElementById('cartContacts');
+  let element=document.getElementById('p');
+  element.textContent=items+ '' +quantity;
+  cart.appendChild(element);
+
 }
 
 // Set up the "submit" event listener on the form.
